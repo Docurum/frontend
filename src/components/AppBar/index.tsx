fb0faaa44489d2c6ce621e5887b95e437c7dda1d
Pinaki Bhattacharjee <pinakipb2@gmail.com>
2023-01-05 01:40:49 +0530

    🚨 Added small linting change

--- src/components/AppBar/index.tsx
+++ src/components/AppBar/index.tsx
@@ -2,14 +2,14 @@ import Logo from "../Logo/Logo";
 import SearchBar from "../SearchBar";
 
 export default function AppBar() {
-    return (
-        <div className="flex flex-row shadow p-4">
-            <div className="flex flex-row items-center">
-            <Logo className="h-10 w-10"/>
-            <div className="text-3xl font-bold text-black">doc</div>
-            <div className="text-3xl font-bold text-blue-600">urum</div>
-            </div>
-            <SearchBar/>
-        </div>
-    )
+  return (
+    <div className="flex flex-row shadow p-4">
+      <div className="flex flex-row items-center">
+        <Logo className="h-10 w-10" />
+        <div className="text-3xl font-bold text-black">doc</div>
+        <div className="text-3xl font-bold text-blue-600">urum</div>
+      </div>
+      <SearchBar />
+    </div>
+  );
 }
