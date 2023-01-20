26c8b8ada09d196ad77115c2b93064f9e4b41c8f
Pinaki Bhattacharjee <pinakipb2@gmail.com>
2023-01-20 21:50:46 +0530

    💄 Made 404 and email-confirm page responsive

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -6,26 +6,29 @@ import summer404 from "../animations/96983-404-error.json";
 import SEO from "../components/SEO";
 
 const FourOhFour = () => {
-  const [theme, setTheme] = useState<any>(summer404);
+  const [theme, setTheme] = useState<any>(null);
   useEffect(() => {
     // Winter theme for Dec, Jan and Feb
     if ([11, 0, 1].includes(new Date().getMonth())) {
       setTheme(winter404);
+    } else {
+      setTheme(summer404);
     }
   }, []);
 
   return (
     <div
       className="w-screen h-screen flex flex-col md:flex-row items-center justify-center gap-x-20 select-none overflow-hidden"
-      style={{ backgroundImage: "radial-gradient( circle 588px at 31.7% 40.2%,  rgba(225,200,239,1) 21.4%, rgba(163,225,233,1) 57.1% )" }}
+      // style={{ backgroundImage: "radial-gradient( circle 588px at 31.7% 40.2%,  rgba(225,200,239,1) 21.4%, rgba(163,225,233,1) 57.1% )" }}
+      style={{ backgroundImage: "linear-gradient( 179.4deg,  rgba(253,240,233,1) 2.2%, rgba(255,194,203,1) 96.2% )" }}
     >
       <SEO title="Page Not Found - Docurum" />
       <div className="w-72 h-72 md:w-96 md:h-96 flex items-center justify-center">
-        <Lottie animationData={theme} play />
+        {theme === null ? <div className="bg-pink-200 h-full w-full animate-pulse"></div> : <Lottie animationData={theme} play />}
       </div>
       <div className="flex flex-col text-center gap-y-4 items-center justify-center">
         <div className="title-font md:mb-4 font-bold text-center flex flex-col justify-center items-center">
-          <h1 className="text-8xl text-blue-600 pb-4">404</h1>
+          <h1 className="text-8xl text-blue-500 pb-4">404</h1>
           <h1 className="text-3xl text-black tracking-wider">Page Not Found</h1>
         </div>
         <p className="px-10 md:px-0 md:mb-4 leading-relaxed text-gray-800 text-lg">...maybe the page you&apos;re looking for is not found or never existed.</p>
--- src/pages/email-confirm/[token].tsx
+++ src/pages/email-confirm/[token].tsx
@@ -1,14 +1,15 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import Lottie from "react-lottie-player";
-
 import successAnimation from "../../animations/89776-success-tick.json";
 import failedAnimation from "../../animations/94303-failed.json";
+import SEO from "../../components/SEO";
 
 const EmailConfirmation = () => {
   const router = useRouter();
   const [isVerified, setIsVerified] = useState<boolean>(false);
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [message, setMessage] = useState<string>("");
   useEffect(() => {
     if (router.isReady) {
       const token = router.query.token as string;
@@ -26,12 +27,13 @@ const EmailConfirmation = () => {
       };
       confirmEmail(token)
         .then((val) => {
-          console.log(val);
           if (val === "S") {
+            setMessage("Your email has been successfully verified!");
             setIsVerified(true);
           }
         })
         .catch((e) => {
+          setMessage("Your verification link has been expired!");
           console.log(e);
         })
         .finally(() => {
@@ -40,28 +42,42 @@ const EmailConfirmation = () => {
     }
   }, [router]);
 
+  const style = {
+    backgroundImage: "radial-gradient( circle farthest-corner at 10% 20%,  rgba(90,92,106,1) 0%, rgba(32,45,58,1) 81.3% )",
+  };
+
   if (isLoading) {
-    return <>Loading...</>;
+    return (
+      <div className="h-screen w-full" style={style}>
+        <SEO title="Email Confirmation - Docurum" />
+        <div className="flex flex-col h-screen justify-center items-center">
+          <div className="w-56 h-56 p-10">
+            <div className="w-full h-full animate-pulse bg-gray-300 rounded-full"></div>
+          </div>
+          <div className=" w-full h-8 px-[15%]">
+            <div className="h-full w-full animate-pulse bg-gray-300 rounded-3xl"></div>
+          </div>
+        </div>
+      </div>
+    );
   }
 
   return (
-    <div className="h-screen w-full bg-red-100">
-      <div className="flex flex-col h-screen justify-center items-center">
-        {isVerified ? (
-          <>
-            <div className="w-36 h-36">
+    <div className="h-screen w-full" style={style}>
+      <SEO title="Email Confirmation - Docurum" />
+      <div className="flex flex-col h-screen justify-center items-center text-center">
+        <div className="w-48 sm:w-56 h-18 sm:h-56">
+          {isVerified ? (
+            <>
               <Lottie loop={false} animationData={successAnimation} play />
-            </div>
-            <div>Your email has beed successfully verified!</div>
-          </>
-        ) : (
-          <>
-            <div className="w-36 h-36">
+            </>
+          ) : (
+            <>
               <Lottie loop={false} animationData={failedAnimation} play />
-            </div>
-            <div>Your verification link has been expired!</div>
-          </>
-        )}
+            </>
+          )}
+        </div>
+        <div className="text-white text-2xl sm:text-3xl px-6 font-semibold">{message}</div>
       </div>
     </div>
   );
